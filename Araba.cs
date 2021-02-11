fa755f1c40f38c454ff57932c2bd78a51bd1dff7
MOHAMED <fardosi1994@gmail.com>
2021-02-11 13:44:21 +0300

    first

---
+++ Araba.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_OOP_EventHandler
+{
+    public delegate void HizAsimiEventHandler();
+    public class Araba
+    {
+        public Araba(int hiz, string model)
+        {
+            this.Hiz = hiz;
+            this.Model = model;
+        }
+
+        // Yeni bir olay tanımlamak için event deyimi kullanılır. Olayları tetiklemek için ise olayın ismi kullanılmaktadır.
+        public event HizAsimiEventHandler HizAsimi;
+        private int _Hiz;
+        public int Hiz
+        {
+            get { return _Hiz; }
+            set
+            {
+                _Hiz = value;
+                if (value > 120)
+                    HizAsimi();
+            }
+        }
+
+        private string _Model;
+        public string Model
+        {
+            get { return _Model; }
+            set { _Model = value; }
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _11_OOP_EventHandler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Events (Olaylar) kullanıcının fare ile tıklaması, klavyeden bir tuşa basma gibi işletim sistemi üzerinden gerçekleştirdiği eylemlerdir. Bir bileşen üzerinde meydana gelen olayları takip eden ve bunları yakalayan mekanizma event handler olarak adlandırılır. 
+
+        // Event Handler ilgili olay gerçekleştiği zaman tetiklenir. Olay gerçekleştikten sonra hangi işlemlerin yapılacağı olay yöneticileri için delegeler ile temsil edilir. Bir kullanıcının fare ile tıklaması örneğin bir Click olayıdır, bir tuşa basma olayı bir Keypress olayıdır ve bu olayların sonucunda nelerin yapılması gerektiğini biz kodlarımızla belirtebiliriz.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lstSonuc.Items.Clear();
+            // Tıpkı delegelerde olduğu gibi bir olayı bir metota bağlamak için "+=" çıkarmak için ise "-=" kullanılır.
+
+            // Yalnız bu noktada dikkat edilmesi gereken durum, olayların bağlanacağı metotların imza yapıları olayın türü olan delege ile aynı olmalıdır. Buradan tabi şunu da anlayabiliriz. Bir eventten önce mutlaka bir delege tanımlanmalıdır. Event önünce access modifier tanımlanabilir, public, private, protected, internal, abstract, new override, static, virtual gibi.
+
+            Araba araba = new Araba(70, "BMW");
+            araba.HizAsimi += new HizAsimiEventHandler(HizLimit);
+
+            for (int i = 0; i < 7; i++)
+            {
+                araba.Hiz += 10;
+                lstSonuc.Items.Add("Arabanın şuanki hızı : " + araba.Hiz);
+            }
+        }
+
+        void HizLimit()
+        {
+            lstSonuc.Items.Add("Araç Hız Limitini Aştı !!!!!");
+        }
+    }
+}
+
